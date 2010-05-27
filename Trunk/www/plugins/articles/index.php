dfc0e5ece5753f067c53c31c90f1f6abe6153964
Mathieu Lory <mathieu@internetcollaboratif.info>
2010-05-27 19:58:20 +0200

    adding licence mention in the header

--- Trunk/www/plugins/articles/index.php
+++ Trunk/www/plugins/articles/index.php
@@ -1,4 +1,22 @@
 <?php
+/*
+Public-Storm
+Copyright (C) 2008-2010 Mathieu Lory <mathieu@internetcollaboratif.info>
+This file is part of Public-Storm.
+
+Public-Storm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Public-Storm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Public-Storm. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 $sPlug = new Settings::$VIEWER_TYPE;
 
@@ -8,48 +26,48 @@ $uri = split('/', $_SERVER['REQUEST_URI']);
 
 if( Settings::getVar('BASE_URL') != "" )
 {
-	$ind = 2;
+$ind = 2;
 }
 else
 {
-	$ind = 1;
+$ind = 1;
 }
 
 
 if ( $uid = $uri[$ind+1] )
 {
-	$articles = articles::getDatas($uid);
-	
-	Settings::setVar('title', $articles{'title'});
-	Settings::setVar('description', '&nbsp;');
-	Settings::setVar('template', 'main.tpl');
-	$sPlug->AddData("title", Settings::getVar('title'));
-	#$sPlug->->AddData("i18n", i18n::getLng());
-	//$sPlug->AddData("article", articles::getArticle($id));
-	$sPlug->AddData("content", $articles{'content'});
-	
-	$breadcrumb = Settings::getVar('breadcrumb');
-	array_push($breadcrumb, array("name" => "Articles", "link" => Settings::getVar('base_url_http')."/articles/"));
-	array_push($breadcrumb, array("name" => Settings::getVar('title')));
-	Settings::setVar('breadcrumb', $breadcrumb);
-	$content = $sPlug->fetch("article.tpl", "plugins/articles");
+$articles = articles::getDatas($uid);
+
+Settings::setVar('title', $articles{'title'});
+Settings::setVar('description', '&nbsp;');
+Settings::setVar('template', 'main.tpl');
+$sPlug->AddData("title", Settings::getVar('title'));
+#$sPlug->->AddData("i18n", i18n::getLng());
+//$sPlug->AddData("article", articles::getArticle($id));
+$sPlug->AddData("content", $articles{'content'});
+
+$breadcrumb = Settings::getVar('breadcrumb');
+array_push($breadcrumb, array("name" => "Articles", "link" => Settings::getVar('base_url_http')."/articles/"));
+array_push($breadcrumb, array("name" => Settings::getVar('title')));
+Settings::setVar('breadcrumb', $breadcrumb);
+$content = $sPlug->fetch("article.tpl", "plugins/articles");
 }
 else
 {
-	$articles = articles::getArticlesTitles();
-	
-	Settings::setVar('title', $articles{'title'});
-	Settings::setVar('description', '&nbsp;');
-	Settings::setVar('template', 'main.tpl');
-	$sPlug->AddData("title", Settings::getVar('title'));
-	#$sPlug->->AddData("i18n", i18n::getLng());
-	//$sPlug->AddData("article", articles::getArticle($id));
-	$sPlug->AddData("articles", $articles);
-	
-	$breadcrumb = Settings::getVar('breadcrumb');
-	array_push($breadcrumb, array("name" => "Articles"));
-	Settings::setVar('breadcrumb', $breadcrumb);
-	$content = $sPlug->fetch("liste_articles.tpl", "plugins/articles");
+$articles = articles::getArticlesTitles();
+
+Settings::setVar('title', $articles{'title'});
+Settings::setVar('description', '&nbsp;');
+Settings::setVar('template', 'main.tpl');
+$sPlug->AddData("title", Settings::getVar('title'));
+#$sPlug->->AddData("i18n", i18n::getLng());
+//$sPlug->AddData("article", articles::getArticle($id));
+$sPlug->AddData("articles", $articles);
+
+$breadcrumb = Settings::getVar('breadcrumb');
+array_push($breadcrumb, array("name" => "Articles"));
+Settings::setVar('breadcrumb', $breadcrumb);
+$content = $sPlug->fetch("liste_articles.tpl", "plugins/articles");
 }
 
 
