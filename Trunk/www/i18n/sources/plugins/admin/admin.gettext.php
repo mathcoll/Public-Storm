bdd80c3483a75289cedaeb2f0e696a31822e8ba6
Mathieu Lory <mathieu@internetcollaboratif.info>
2010-05-30 18:27:38 +0200

    Mise à jour des traductions FR

--- Trunk/www/i18n/sources/plugins/admin/admin.gettext.php
+++ Trunk/www/i18n/sources/plugins/admin/admin.gettext.php
@@ -1,9 +1,25 @@
 <?php
 gettext("liste_plugins");
+gettext("Nom");
+gettext("Description");
+gettext("Statut");
+gettext("Version");
+gettext("Date de mise à jour");
+gettext("Lien de mise à jour");
+gettext("by");
+gettext("Activer");
+gettext("Désactiver");
 
 ?><?php
 gettext("list_users");
 gettext("Pages :");
+gettext("Nom");
+gettext("Prénom");
+gettext("Login");
+gettext("Email");
+gettext("Lang");
+gettext("Inscription");
+gettext("Mise à jour");
 
 ?><?php
 gettext("forbidden_details");
