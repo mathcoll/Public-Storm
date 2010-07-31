0104c425b16d89b37ea2e7baf79b3bb0faaad2c4
Mathieu Lory <mathieu@internetcollaboratif.info>
2010-07-31 13:37:32 +0200

    configuration de "dot" par défaut pour la génération des graphviz

--- Trunk/www/conf/public_storm.php
+++ Trunk/www/conf/public_storm.php
@@ -1,6 +1,6 @@
 <?php
 
-Settings::setVar('graphviz_type', 'neato', 'public_storm', 'GraphViz command');
+Settings::setVar('graphviz_type', 'dot', 'public_storm', 'GraphViz command');
 Settings::setVar('neato_type', 'jpg', 'public_storm', 'type of the neato image file generated');
 Settings::setVar('storms_per_page', '30', 'public_storm', 'number of storm printed on each page');
 Settings::setVar('user_storms_per_page', '15', 'public_storm', 'number of storm printed on each user page');
