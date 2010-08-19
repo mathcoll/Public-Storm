ce49083e2975361eaee05d700a584c20827ef403
Mathieu Lory <mathieu@internetcollaboratif.info>
2010-08-19 20:53:58 +0200

    Correction url relative

--- Trunk/www/plugins/meteor/_plugin.php
+++ Trunk/www/plugins/meteor/_plugin.php
@@ -29,8 +29,8 @@ final class meteor extends Plugins
 	{
 		require(Settings::getVar('prefix') . 'conf/'.self::$name.'.php');
 		//require_once('./plugins/'.self::$name.'/classes/'.self::$name.'.php');
-		Settings::addCss('screen', Settings::getVar('BASE_URL').'/plugins/'.self::$name.'/styles/'.self::$name.'.css');
-		Settings::addCss('screen', Settings::getVar('theme_dir').'plugins/'.self::$name.'/styles/admin.css', 'admin.css');
+		Settings::addCss('screen', Settings::getVar('theme_dir_http').'/plugins/'.self::$name.'/styles/'.self::$name.'.css');
+		Settings::addCss('screen', Settings::getVar('theme_dir_http').'plugins/'.self::$name.'/styles/admin.css', 'admin.css');
 		Settings::addJs('text/javascript', 'http://meteor.internetcollaboratif.info/meteor.js');
 		Settings::addJs('text/javascript', Settings::getVar('theme_dir').'plugins/'.self::$name.'/scripts/'.self::$name.'.js');
 	}
