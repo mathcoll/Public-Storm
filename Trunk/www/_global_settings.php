bce01f4b7791ca9cafb325fa1236cd69ace7d7fa
Mathieu Lory <mathieu@internetcollaboratif.info>
2011-03-24 22:49:33 +0100

    nouvelle release 11.3.24

--- Trunk/www/_global_settings.php
+++ Trunk/www/_global_settings.php
@@ -36,7 +36,7 @@ Settings::setVar('SITE_BASELINE', '', 'admin', 'Site baseline, also defined in t
 Settings::setVar('SITE_DESCRIPTION', '', 'admin', 'Site description, also defined in the languages files');
 
 // Current version
-Settings::setVar('SITE_VERSION', '11.2.23');
+Settings::setVar('SITE_VERSION', '11.3.24');
 
 // Debug switch. Set it to true for output additional information.
 define('DEBUG', false);
