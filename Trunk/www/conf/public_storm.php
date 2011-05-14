a62ed84c028f4d49752ba944231a3e9a0803b5a6
Mathieu Lory <mathieu@internetcollaboratif.info>
2011-05-14 21:28:31 +0200

    nicer config

--- Trunk/www/conf/public_storm.php
+++ Trunk/www/conf/public_storm.php
@@ -3,6 +3,6 @@
 Settings::setVar('graphviz_type', 'dot', 'public_storm', 'GraphViz command');
 Settings::setVar('neato_type', 'jpg', 'public_storm', 'type of the neato image file generated');
 Settings::setVar('storms_per_page', '30', 'public_storm', 'number of storm printed on each page');
-Settings::setVar('user_storms_per_page', '15', 'public_storm', 'number of storm printed on each user page');
+Settings::setVar('user_storms_per_page', '20', 'public_storm', 'number of storm printed on each user page');
 
 ?>
